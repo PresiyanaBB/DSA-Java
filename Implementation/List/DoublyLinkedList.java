8ed4abc3c990673ad94c6c3b59e5b1f8e7395383
Presiyana Borisova <presiana03@gmail.com>
2025-04-06 18:40:26 +0300

    DoublyLinkedList added

---
+++ Implementation/List/DoublyLinkedList.java
@@ -0,0 +1,68 @@
+package Implementation.List;
+
+public class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+
+    private static class Node {
+        int data;
+        Node prev;
+        Node next;
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+    }
+
+    public void delete(int data) {
+        Node curr = head;
+        while (curr != null) {
+            if (curr.data == data) {
+                if (curr.prev != null) curr.prev.next = curr.next;
+                else head = curr.next;
+                if (curr.next != null) curr.next.prev = curr.prev;
+                else tail = curr.prev;
+                return;
+            }
+            curr = curr.next;
+        }
+    }
+
+    public boolean contains(int data) {
+        Node curr = head;
+        while (curr != null) {
+            if (curr.data == data) return true;
+            curr = curr.next;
+        }
+        return false;
+    }
+
+    public void printForward() {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    public void printBackward() {
+        Node curr = tail;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.prev;
+        }
+        System.out.println();
+    }
+}
+
